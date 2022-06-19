6ae93c119ec78440bafa59181d89ed9212bf5d90
Steve Gounoue <gounoue@l3s.de>
2022-06-20 00:24:25 +0200

    initial files

---
+++ ml_template/configuration.py
@@ -0,0 +1,133 @@
+import configargparse
+import copy
+from postgre_db import table_exists, db_types, get_col_names, format_value
+
+class Configuration:
+
+    def __init__(self):
+        self._parser = configargparse.ArgumentParser()
+
+        # options parsed by the default parser
+        self._options = None
+
+        # individual configurations for different runs
+        self._configs= []
+
+        # arguments with more than one value
+        self._multivalue_args = []
+
+        # data types
+        self._data_types = {}
+
+        self._parser.add_argument("-d", "--defaultConfig", is_config_file=True, help="Default Config file")
+        self._parser.add_argument("-c", "--config", is_config_file=True, help="Config file")
+
+        # setup storage of results
+        self._parser.add_argument("-dbH", "--dbHost", is_config_file=False, help="URL of postgre db")
+        self._parser.add_argument("-dbU", "--dbUser", is_config_file=False, help="User of postgre db")
+        self._parser.add_argument("-dbP", "--dbPassword", is_config_file=False, help="Password of postgre db")
+        self._parser.add_argument("-dbN", "--dbName", is_config_file=False, help="Name of postgre db")
+        self._parser.add_argument("-dbS", "--dbSchema", is_config_file=False, help="Schema to use in postgre db")
+        self._parser.add_argument("-ct", "--configTable", is_config_file=False, help="Table to store configurations of runs")
+        self._parser.add_argument("-srt", "--speedResultTable", is_config_file=False, help="Table to store speed results of runs")
+        self._parser.add_argument("-lrt", "--labelResultTable", is_config_file=False, help="Table to store label results of runs")
+        self._parser.add_argument("-crt", "--classificationResultTable", is_config_file=False, help="Table to store classification results of runs")
+        self._parser.add_argument("-rV", "--resultView", is_config_file=False, help="Name of view of joint config result tables")
+
+
+    def add_entry(self, short, long, help, type=str, nargs='*'):
+        self._parser.add("-"+short, "--"+long, help=help, is_config_file=False, type=type, nargs=nargs)
+        self._data_types[long.lower()] = type
+
+    def add_model_entry(self, long, help, action='store_false'):
+        self._parser.add("--"+long, help=help, is_config_file=False, action=action)
+        self._data_types[long.lower()] = bool
+
+    def parse(self):
+        self._options = self._parser.parse_args()
+
+        # find values with more than one entry
+        dict_options = vars(self._options)
+        for k in dict_options :
+            if isinstance(dict_options[k], list):
+                self._multivalue_args.append(k)
+
+        self._configs.append(self._options)
+        for ma in self._multivalue_args:
+            new_configs = []
+
+            # in each config
+            for c in self._configs:
+                # split each attribute with multiple values
+                for v in dict_options[ma]:
+                    current = copy.deepcopy(c)
+                    setattr(current, ma, v)
+                    new_configs.append(current)
+
+            # store splitted values
+            self._configs = new_configs
+
+    def create_config_table(self, cur):
+        config_exists = table_exists(cur, self._options.configTable, self._options.dbSchema)
+
+        if not config_exists:
+            query = "CREATE TABLE "+self._options.configTable+" (" +\
+                "    id serial primary key"
+
+            for col in self._data_types:
+                query += ", "+col+" "+db_types(self._data_types[col])
+
+            query += ", start timestamp WITHOUT TIME ZONE default now())"
+
+            cur.execute(query)
+        else:
+            col_names = get_col_names(cur, self._options.configTable, self._options.dbSchema)
+            missing_rows = [x for x in self._data_types if x not in col_names]
+
+            for mr in missing_rows:
+                query = "ALTER TABLE " + self._options.configTable +" ADD COLUMN " + mr +" " + db_types(self._data_types[mr]) + ";"
+                cur.execute(query)
+
+    def setup_db(self, db):
+        # create config tables
+
+        with db.get_connection() as conn:
+            with conn.cursor() as cur:
+                self.create_config_table(cur)
+
+        for c in self._configs:
+            c.db = db
+
+    def get_configs(self):
+        return self._configs
+
+
+def register_run(config):
+
+    lower_to_capitalized = {}
+    for v in vars(config):
+        lower_to_capitalized[v.lower()] = v
+
+    with config.db.get_connection() as conn:
+        with conn.cursor() as cur:
+            col_names = get_col_names(cur, config.configTable, config.dbSchema)
+
+            # remove internal columns
+            col_names.remove("id")
+            col_names.remove("start")
+
+            query = "INSERT INTO "+config.configTable+" (" + (",".join(col_names)) + ") VALUES "
+            values = []
+            for col in col_names:
+                attr = (getattr(config, lower_to_capitalized[col]))
+
+                attr = format_value(attr)
+
+                values.append(attr)
+
+            query += "("+(",".join(values))+")"
+            query += " RETURNING id"
+
+            cur.execute(query)
+            id = cur.fetchall()[0][0]
+    return id
---
+++ ml_template/evaluation.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+from postgre_db import table_exists, db_types, get_col_names, format_value
+
+
+class Evaluation:
+
+    def __init__(self, config):
+        self.result_types = {
+            "speed": "speed",
+            "label": "label",
+            "classification": "classification"
+        }
+        self.table_name = {
+            self.result_types["speed"]: config.speedResultTable,
+            self.result_types["label"]: config.labelResultTable,
+            self.result_types["classification"]: config.classificationResultTable
+        }
+        self.metrics = {
+            self.result_types["speed"]: {},
+            self.result_types["label"]: {},
+            self.result_types["classification"]: {}
+        }
+        self.data_types = {
+            self.result_types["speed"]: {},
+            self.result_types["label"]: {},
+            self.result_types["classification"]: {}
+        }
+        self.config = config
+
+    def add_metric(self, name, type, result_type="speed"):
+        self.data_types[result_type][name] = type
+
+    def compute_metric(self, metrics, bestid=0, time_spent=0, weights=""):
+        # I use this function for speed
+        self.metrics[self.result_types["speed"]]["mae"] = metrics[0]
+        self.metrics[self.result_types["speed"]]["mape"] = metrics[1]
+        self.metrics[self.result_types["speed"]]["rmse"] = metrics[2]
+        self.metrics[self.result_types["speed"]]["bestid"] = bestid
+        self.metrics[self.result_types["speed"]]["time_spent"] = time_spent
+        self.metrics[self.result_types["speed"]]["weights"] = weights
+
+    def compute_label_metric(self, metrics, bestid=0, time_spent=0):
+        # I use this function for speed
+        self.metrics[self.result_types["label"]]["l_mae"] = metrics[0]
+        self.metrics[self.result_types["label"]]["l_mape"] = metrics[1]
+        self.metrics[self.result_types["label"]]["l_rmse"] = metrics[2]
+        self.metrics[self.result_types["label"]]["l_bestid"] = bestid
+        self.metrics[self.result_types["label"]]["l_time_spent"] = time_spent
+
+    def compute_classification_metric(self, confusion_matrix, precision, recall, f_score):
+        # I use this function for speed
+        num_concepts = confusion_matrix.shape[0]
+        for i in range(num_concepts):
+            self.metrics[self.result_types["classification"]][f"precision_{i}"] = precision[i]
+            self.metrics[self.result_types["classification"]][f"recall_{i}"] = recall[i]
+            self.metrics[self.result_types["classification"]][f"f_score_{i}"] = f_score[i]
+            for j in range(num_concepts):
+                self.metrics[self.result_types["classification"]][f"cm_{i}{j}"] = confusion_matrix[i, j]
+
+    def create_tables(self):
+        with self.config.db.get_connection() as conn:
+            with conn.cursor() as cur:
+                # put a loop for each result_type
+                for result_type in list(self.result_types.values()):
+                    if not table_exists(cur, self.table_name[result_type], self.config.dbSchema):
+                        create_query = "CREATE TABLE " + self.table_name[result_type] + " (" + \
+                                       "config integer primary key references " + self.config.configTable + " "
+
+                        for m in self.data_types[result_type]:
+                            create_query += ", " + m + " " + db_types(self.data_types[result_type][m])
+                        create_query += ");"
+
+                        cur.execute(create_query)
+                    else:
+                        col_names = get_col_names(cur, self.table_name[result_type], self.config.dbSchema)
+                        missing_rows = [x for x in self.data_types[result_type] if x not in col_names]
+
+                        for mr in missing_rows:
+                            query = "ALTER TABLE " + self.table_name[
+                                result_type] + " ADD COLUMN " + mr + " " + db_types(
+                                self.data_types[result_type][mr]) + ";"
+                            cur.execute(query)
+
+                    try:
+                        view_query = "CREATE OR REPLACE VIEW " + f"view_{result_type}" + " as " + \
+                                     "SELECT * FROM " + self.config.configTable + " c " + \
+                                     " JOIN " + self.table_name[result_type] + " r on (c.id = r.config)" + \
+                                     ";"
+
+                        cur.execute(view_query)
+                    except Exception as e:
+                        raise Exception
+
+    def store_results(self, result_type="speed"):  # result_type = "speed", "label", "classification"
+        self.create_tables()
+        with self.config.db.get_connection() as conn:
+            with conn.cursor() as cur:
+
+                cols = list(self.data_types[result_type].keys())
+                query = "INSERT INTO " + self.table_name[result_type] + " (config," + (",".join(cols)) + ") VALUES ("
+                try:
+                    values = [format_value(self.config.id)]
+                    for c in cols:
+                        v = format_value(self.metrics[result_type][c])
+                        values.append(v)  # v is of type tensor
+                except Exception as e:
+                    raise Exception
+
+                query += ",".join(values)
+                query += ")"
+
+                cur.execute(query)
